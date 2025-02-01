8ab19bf563ce30c7c41df43f2ad5f8aebd15a17e
sourav <devbhsourav@gmail.com>
2025-02-01 22:37:02 +0530

    [sourav]add : linearSearch

---
+++ src/searching/LinearSearch.java
@@ -0,0 +1,22 @@
+package searching;
+
+public class LinearSearch {
+
+	public void linarSearchAlgo(int[] arr, int checkingValue) {
+		int index = 0;
+		for(int ar : arr) {
+			if(ar == checkingValue) {
+				System.out.print("Value found in Array " + checkingValue + " & index is at " + index);
+				return;
+			}
+			index++;
+		}
+		System.out.print("Value is not present in the array");
+	}
+	public static void main(String[] args) {
+		
+		int[] intArr = {3, 6, 5, 9, 3, 2,99};
+		LinearSearch linearSearch = new LinearSearch();
+		linearSearch.linarSearchAlgo(intArr, 9);
+	}
+}
