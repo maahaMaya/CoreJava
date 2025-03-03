cb07babb9f54b809f4af375c2ff5e057d76b885e
sourav <devbhsourav@gmail.com>
2025-03-03 23:50:32 +0530

    [sourav]add : SourceToDestination

--- backtracking/ChessBoardAllPossibleRuleCombination.java
+++ backtracking/ChessBoardAllPossibleRuleCombination.java
@@ -12,7 +12,7 @@ public class ChessBoardAllPossibleRuleCombination {
         for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i-- , j--) {
             if(board[i][j] =='Q') {
                 return false;
-            }
+            }s
         }
         
         // diagonal right up
---
+++ backtracking/SourceToDestination.java
@@ -0,0 +1,22 @@
+
+public class SourceToDestination {
+    
+    public static int gridWays(int i, int j, int n, int m) {
+    //base case
+    if(i == n-1 && j == m-1) {//condition for last cell
+        return 1;
+    }
+    else if(i == n || j == n){
+        return 0;
+    }
+        
+    int w1 = gridWays (i+1, j, n, m);
+    int w2 = gridWays(i, j+1, n, m);
+    return w1 + w2;
+    }
+    
+    public static void main(String args[]) {
+        int n = 3, m = 3;
+        System. out. println("Total Ways to reach Source to Destination is : "  + gridWays(0, 0, n, m)) ;
+    }
+}
